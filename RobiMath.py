57ed265a1b99f71d8df468125f34629bab10c384
treeshadas <113580966+treeshadas@users.noreply.github.com>
2024-11-07 14:27:15 +0600

    Add files via upload

--- RobiMath.py
+++ RobiMath.py
@@ -1,1 +1,10 @@
 from mpmath import mp
+def print_pi_to_n_digits(n):
+
+    # Set precision to n decimal places
+
+    mp.dps = n + 1  # Adding 1 to account for the "3." before the decimals
+
+    # Return pi to n decimal places as a string
+
+    return str(mp.pi)
